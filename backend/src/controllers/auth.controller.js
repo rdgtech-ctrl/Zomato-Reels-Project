fb3f3f93753524aaef7594a9c95191e06dea5d43
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-01-06 23:20:20 +0530

    Update files

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 // logic inside apis are created over here
+// login api is created in here
 
 const userModel = require("../models/user.model")
 const bcrypt = require('bcryptjs');
@@ -27,44 +28,57 @@ async function registerUser(req, res) {
     })
 
     const token = jwt.sign({
-        id:user._id,
-    },"d040c75e32d73b26c0b19063decd026d")
+        id: user._id,
+    }, process.env.JWT_SECRET)
 
-    res.cookie("token",token)
+    res.cookie("token", token)
 
     res.status(201).json({
-        message:"User registered successfully",
-        user:{
-            _id:user._id,
-            email:user.email,
-            fullName:user.fullName
+        message: "User registered successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
         }
     })
 }
 
-async function loginUser(req,res){
-    const{email,password} = req.body;
+async function loginUser(req, res) {
+    const { email, password } = req.body;
 
-    const user = await uswerModel.findOne({
+    const user = await userModel.findOne({
         email
     })
-    if(!user){
-        res.status(400).json({
-            message:"Invalid email or password"
-        })
-    }
-
-    const isPasswordValid = await bcrypt.compare(password,user.password);
-
-    if(!isPasswordValid){
+    if (!user) {
         return res.status(400).json({
-            message:"Invalid email or password"
+            message: "Invalid email or password"
         })
     }
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
+        }
+    })
 }
 
-module.exports={
+module.exports = {
     registerUser,
     loginUser
 }
