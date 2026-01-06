d5fa6feee1053f201236a128d1517aedae484d6b
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-01-06 23:28:05 +0530

    Update files

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // logic to how you connect your server to your database
 function connectDB() {
-    mongoose.connect("mongodb://localhost:27017/food-view")
+    mongoose.connect(process.env.MONGODB_URI)
         .then(() => {
             console.log("MongoDB connected");
         })
