4abf37eac41a6747426f9d4fa99b1c363f6b80d0
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-01-08 00:15:25 +0530

    Update files

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,10 +1,38 @@
 const foodModel = require('../models/food.model');
+const storageService = require('../services/storage.service');
+const { v4: uuid } = require("uuid")
 
 async function createFood(req, res) {
-    console.log(req.foodPartner);
+    try {
+        console.log("Food Partner:", req.foodPartner);
+        console.log("Body:", req.body)
+        console.log("File:", req.file)
 
-    res.send("food item created")
+        if (!req.file) {
+            return res.status(400).json({
+                message: "No file uploaded"
+            })
+        }
+
+        console.log("File buffer:", req.file.buffer);
+        
+        const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid())
+
+        console.log("Upload result:", fileUploadResult)
+
+        res.status(201).json({
+            message: "Food item created",
+            data: fileUploadResult
+        })
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({
+            message: "Error creating food",
+            error: error.message
+        })
+    }
 }
+
 module.exports = {
     createFood
 }
--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -2,8 +2,17 @@ const express = require('express');
 const foodController = require("../controllers/food.controller")
 const authMiddleware = require("../middlewares/auth.middleware")
 const router = express.Router();
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+
 
 /*POST /api/food/  [protected]*/
-router.post('/', authMiddleware.authFoodPartnerMiddleware, foodController.createFood)
+router.post('/',
+    authMiddleware.authFoodPartnerMiddleware, upload.single("video"),
+    foodController.createFood)
 
 module.exports = router
